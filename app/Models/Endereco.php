703978921f238d855d34f5ec4647e0b53c63b76c
Junior freitas <juninhofreitas2@hotmail.com>
2019-05-09 14:23:20 -0300

    model endereco

---
+++ app/Models/Endereco.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+use Core\BaseModel;
+
+class Endereco extends BaseModel
+{
+
+    protected $primaryKey = 'end_id';
+
+    protected $table = 'enderecos';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'end_cep',
+		'end_logradouro',
+		'end_numero',
+		'end_bairro',
+		'end_estado',
+		'end_cidade'
+    ];
+
+}
